728ebdd60dc2c6c79f2b15ca546383dae4cf37ef
Hannah Taylor Anderson <handerso@lxplus7116.cern.ch>
2023-04-30 21:19:21 +0200

    TRD_Build added

---
+++ TRD_Build/CMakeFiles/TRD.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TRD.dir/TRD.cc.o"
+  "CMakeFiles/TRD.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/TRD.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/TRD.dir/src/EventAction.cc.o"
+  "CMakeFiles/TRD.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/TRD.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/TRD.dir/src/RunAction.cc.o"
+  "CMakeFiles/TRD.dir/src/SteppingAction.cc.o"
+  "TRD"
+  "TRD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TRD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
